98895d5e5f31a0b43c84f58344e783c0d99a133b
Yuki Watanabe <ukwhatn@gmail.com>
2023-01-01 01:23:16 +0900

    [add feature]複数カテゴリを跨いだChソート機能を実装

--- bot/cogs/Util.py
+++ bot/cogs/Util.py
@@ -1,3 +1,6 @@
+import math
+import re
+
 import discord
 from discord.commands import slash_command
 from discord.ext import commands
@@ -17,6 +20,40 @@ class Util(commands.Cog):
 
         for i, ch in enumerate(chs_in_category):
             await ch.edit(position=i)
+
+        await ctx.respond("完了しました", ephemeral=True)
+
+    @slash_command(name="sort_multi_categories", description="複数カテゴリ内のChを名前順にソートします")
+    @commands.is_owner()
+    async def sort_multi_categories(
+            self, ctx: discord.commands.context.ApplicationContext,
+            name_contains: str
+    ):
+        await ctx.respond("開始します", ephemeral=True)
+
+        categories = [category for category in ctx.guild.categories if name_contains.lower() in category.name.lower()]
+        categories.sort(key=lambda category: category.name)
+
+        channels = [channel for category in categories for channel in category.channels]
+        channels.sort(key=lambda channel: channel.name)
+
+        sort_result = [[] for _ in range(len(categories))]
+        for i, channel in enumerate(channels, 1):
+            sort_result[math.floor(i / 50)].append(channel)
+
+        for i, category in enumerate(categories):
+            # 同一パラメータでカテゴリを作成
+            new_category = await ctx.guild.create_category(
+                name=category.name,
+                overwrites=category.overwrites,
+                position=category.position,
+            )
+
+            for j, channel in enumerate(sort_result[i]):
+                await channel.edit(category=new_category, position=j)
+
+            if len(category.channels) == 0:
+                await category.delete()
 
         await ctx.respond("完了しました", ephemeral=True)
 
